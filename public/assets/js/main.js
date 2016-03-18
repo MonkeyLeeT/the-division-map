8ae7d2a45f9721ff75812af843c1cb746a3eefff
Michael Lynch <mlynch@cardinalsolutions.com>
2016-03-17 23:15:19 -0400

    update images

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,6 @@
 var theDivisionMap = L.map('map-content', {
-    center: [0, 0],
-    zoom: 2
+    center: [-70, 50],
+    zoom: 4
 });
 
 L.tileLayer('/assets/img/map/{z}/{x}/{y}.png', {
@@ -33,19 +33,20 @@ var DivisionIcon = L.Icon.extend({
 });
 
 var Icons = {
-    Extractions:        new DivisionIcon({iconUrl: 'leaf-green.png'}),
-    SubwayEnterances:   new DivisionIcon({iconUrl: 'leaf-green.png'}),
-    Landmarks:          new DivisionIcon({iconUrl: 'leaf-green.png'}),
-    SafeHouses:         new DivisionIcon({iconUrl: 'leaf-green.png'}),
-    Checkpoints:        new DivisionIcon({iconUrl: 'leaf-green.png'}),
-    Lootable: {
-        DivisionTech:   new DivisionIcon({iconUrl: 'leaf-green.png'}),
-        DarkzoneChests: new DivisionIcon({iconUrl: 'leaf-green.png'}),
+    'Extractions':        new DivisionIcon({iconUrl: 'leaf-green.png'}),
+    'SubwayEnterances':   new DivisionIcon({iconUrl: 'leaf-green.png'}),
+    'Landmarks':          new DivisionIcon({iconUrl: '/assets/img/icons/landmark.png'}),
+    'SafeHouses':         new DivisionIcon({iconUrl: 'leaf-green.png'}),
+    'Checkpoints':        new DivisionIcon({iconUrl: '/assets/img/icons/checkpoint.png'}),
+    'DZEnterance':        new DivisionIcon({iconUrl: '/assets/img/icons/dz-enterance.png'}),
+    'Lootable': {
+        'DivisionTech':   new DivisionIcon({iconUrl: 'leaf-green.png'}),
+        'DarkzoneChests': new DivisionIcon({iconUrl: 'leaf-green.png'}),
     },
-    Enemy: {
-        Champions:      new DivisionIcon({iconUrl: 'leaf-green.png'}), // Named Bosses
-        Elites:         new DivisionIcon({iconUrl: 'leaf-green.png'}), // Yellow
-        Rares:          new DivisionIcon({iconUrl: 'leaf-green.png'})  // Purple
+    'Enemy': {
+        'Champions':      new DivisionIcon({iconUrl: 'leaf-green.png'}), // Named Bosses
+        'Elites':         new DivisionIcon({iconUrl: 'leaf-green.png'}), // Yellow
+        'Rares':          new DivisionIcon({iconUrl: 'leaf-green.png'})  // Purple
     }
 };
 
@@ -53,15 +54,28 @@ var Icons = {
 // Markers
 //
 
-// var marker = L.marker([90, 180]);
-// marker.bindPopup("Division Tech");
-// marker.on('mouseover', function (e) {
-//     this.openPopup();
-// });
-// marker.on('mouseout', function (e) {
-//     this.closePopup();
-// });
-// marker.addTo(theDivisionMap);
+var Markers = [
+    { type: "Checkpoints", label: "Checkpoint", locations: [
+        {x: -78.5, y: 33.6}, // dz01 south entrance
+        {x: -72.8, y: 83.8}, // dz01 east entrance
+        {x: -72.8, y: -14.8}, // dz01 west entrance
+    ]}
+];
+
+_.each(Markers, function(marker){
+    var icon = _.get(Icons, marker.type);
+    _.each(marker.locations, function(loc){
+        var marker = L.marker([loc.x, loc.y], {icon: icon});
+        // marker.bindPopup(marker.label);
+        // marker.on('mouseover', function (e) {
+        //     this.openPopup();
+        // });
+        // marker.on('mouseout', function (e) {
+        //     this.closePopup();
+        // });
+        marker.addTo(theDivisionMap);
+    });
+});
 
 //
 // Polygons
@@ -73,18 +87,3 @@ var Areas = {
 
 // var polygon = L.polygon(Areas.dz01, {clickable: false, weight: 1, fillOpacity: 0.1});
 // polygon.addTo(theDivisionMap);
-
-//
-// Custom Icons
-//
-
-// var greenIcon = L.icon({
-//     iconUrl: 'leaf-green.png',
-//     shadowUrl: 'leaf-shadow.png',
-//     iconSize:     [38, 95], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
-// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
