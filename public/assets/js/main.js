fa159af2ed5b809309e653d2ef0b3e47407eda2d
Michael Lynch <mlynch@cardinalsolutions.com>
2016-03-18 18:09:28 -0400

    Update POI

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,10 @@ var theDivisionMap = L.map('map-content', {
     center: [-60, 20],
     zoom: 3
 });
+// var theDivisionMap = L.map('map-content', {
+//     center: [-10, 20],
+//     zoom: 4
+// });
 
 L.tileLayer('/assets/img/map/{z}/{x}/{y}.jpg', {
     attribution: '',
@@ -41,13 +45,13 @@ var Icons = {
     'DZEnterances':       new DivisionIcon({iconUrl: '/assets/img/icons/dz-enterance.png'}),
     'Containment':        new DivisionIcon({iconUrl: '/assets/img/icons/containment.png'}),
     'Lootable': {
-        'DivisionTech':   new DivisionIcon({iconUrl: 'leaf-green.png'}),
-        'DarkzoneChests': new DivisionIcon({iconUrl: 'leaf-green.png'}),
+        'DivisionTech':   new DivisionIcon({iconUrl: '/assets/img/icons/division-tech.png'}),
+        'DarkzoneChests': new DivisionIcon({iconUrl: '/assets/img/icons/'}),
     },
     'Enemy': {
-        'Champions':      new DivisionIcon({iconUrl: 'leaf-green.png'}), // Named Bosses
-        'Elites':         new DivisionIcon({iconUrl: 'leaf-green.png'}), // Yellow
-        'Rares':          new DivisionIcon({iconUrl: 'leaf-green.png'})  // Purple
+        'Champions':      new DivisionIcon({iconUrl: '/assets/img/icons/'}), // Named Bosses
+        'Elites':         new DivisionIcon({iconUrl: '/assets/img/icons/'}), // Yellow
+        'Rares':          new DivisionIcon({iconUrl: '/assets/img/icons/'})  // Purple
     }
 };
 
@@ -74,15 +78,42 @@ var Markers = [
         {x: -66.10,  y: 54.00,   label: "Abandoned Gas Station"},
         {x: -58.70,  y: 48.00,   label: "Construction Site"},
         {x: -41.80,  y: 25.30,   label: "Kalkesse Sporting Store"},
-        {x: -41.80,  y: 67.0,    label: "The Library"},
+        {x: -41.80,  y: 67.00,   label: "The Library"},
+        {x: -13.40,  y: 32.10,   label: "Refueling Station"},
+        {x: 8.80,    y: -4.7,    label: "Arch Plaza"},
     ]},
     { type: "Extractions", locations: [
         {x: -70.00,  y: 65.00,  label: "Gas Station Extraction"},
         {x: -72.30,  y: -9.50,  label: "Subway Extraction"},
         {x: -51.60,  y: 12.10,  label: "Rooftop Extraction"},
+        {x: -12.80,  y: -6.6,    label: "Bryant Park Extraction"},
     ]},
     { type: "SafeHouses", locations: [
         {x: -45.50,  y: 50.00,  label: "DZ02 Safe Room"},
+        {x: -35.20,  y: -4.20,  label: "DZ03 Safe Room"},
+        {x: 25.9,    y: -1.70,  label: "DZ04 Safe Room"},
+    ]},
+    { type: "Lootable.DivisionTech", locations: [
+        {x: 9.5,    y: 51.8,  label: "Div Tech Room"},
+        {x: 30.9,   y: 26.5,  label: "Div Tech Room"},
+        {x: -63,    y: 34,    label: "On Scaffolding"},
+        {x: -60.3,  y: 49.6,  label: "Second Floor"},
+        {x: -57.0,  y: 49.6,  label: "Second Floor"},
+        {x: -58.7,  y: 48.0,  label: "Third Floor"},
+        {x: -53.5,  y: 77.0,  label: "????"},
+        {x: -51.5,  y: 77.0,  label: "????"},
+        {x: -53.5,  y: 74.0,  label: "????"},
+        {x: -41.0,  y: 63.0,  label: "On Ground"},
+        {x: -39.0,  y: 28,    label: "Third Floor: Behind Christmas Tree"},
+        {x: -44.3,  y: 28,    label: "Second Floor: Billards Room"},
+        {x: -44.7,  y: 20,    label: "Third Floor: On Scaffolding"},
+        {x: -43.3,  y: 23,    label: "First Floor"},
+        {x: -30,  y: 12.5,    label: "????"},
+        {x: -30,  y: 2,       label: "????"},
+        {x: -24,  y: 50,      label: "????"},
+        {x: -13.4, y: 30.5,   label: "????"},
+        {x: -6,  y: 6.5,      label: "????"},
+        {x: 8.80,  y: -4.7,   label: "????"},
     ]},
 ];
 
